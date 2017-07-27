0207774f1b6720994de3b74b53fa26e6f6810e17
Ginny <ginny.pennekamp@gmail.com>
2017-07-26 20:36:53 -0700

    Redo difference-of-squares

--- 04difference-of-squares/Sources/DifferenceOfSquares.swift
+++ 04difference-of-squares/Sources/DifferenceOfSquares.swift
@@ -5,11 +5,7 @@ class Squares {
 
     let rangeMax: Int
     
-    varimport Foundation
-    
-    // MARK: - Helper Methods
-    
-    func findSumOfSquares(_ rangeMax: Int) -> Int {
+    var sumOfSquares: Int {
         let setRange: [Int] = Array(1...rangeMax)
         // square all the numbers in range
         let squaredRange: [Int] = setRange.map { $0 * $0 }
@@ -18,7 +14,7 @@ class Squares {
         return sumSquares
     }
     
-    func findSquareOfSums(_ rangeMax: Int) -> Int {
+    var squareOfSums: Int {
         let setRange: [Int] = Array(1...rangeMax)
         // add all numbers in range
         let sumRange: Int = setRange.reduce(0) { $0 + $1 }
@@ -26,27 +22,13 @@ class Squares {
         let squaredSum = sumRange * sumRange
         return squaredSum
     }
-    
-    // MARK: - Squares
-    
-    class Squares {
         
-        var sumOfSquares: Int
-        var squareOfSums: Int
-        
-        var differenceOfSquares: Int {
-            return self.squareOfSums - self.sumOfSquares
-        }
-        
-        init(_ rangeMax: Int) {
-            self.sumOfSquares = findSumOfSquares(rangeMax)
-            self.squareOfSums = findSquareOfSums(rangeMax)
-        }
+    var differenceOfSquares: Int {
+        return squareOfSums - sumOfSquares
     }
-    
+        
     init(_ rangeMax: Int) {
         self.rangeMax = rangeMax
     }
-
 
 }
